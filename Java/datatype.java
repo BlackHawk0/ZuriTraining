45e5fc26f3b3a6cad51318d40a038033a2da9b81
mogul <mogul@mogul.mogul>
2021-03-29 13:36:22 +0300

    initial commit

---
+++ Java/datatype.java
@@ -0,0 +1,55 @@
+//primitive
+
+//float
+//short < 32000
+//int
+//double
+//long 
+
+
+//non-primitive
+
+//character - single character words
+//boolean - either true or flase
+//byte
+
+
+class Main {
+    public static void main(String[] args) {
+      //declaring strings
+      String daniel = "Daniel. Wow, that's my name and it's";
+  
+      //declaring booleans
+      boolean dan = true;
+      System.out.println("Boolean :" + dan);
+  
+      //declaring intergers
+      int dante = 20;
+      System.out.println("Interger :" + dante);
+  
+      //declaring doubles
+      double dob = 120.99;
+      System.out.println("Double :" + dob);
+  
+      //declaring float
+      float flot = 2f;
+      System.out.println("Float : " + flot);
+  
+  
+      //declaring characters
+  
+      char character = 'a';
+      System.out.println("Character :" + character);
+  
+      System.out.println(daniel + dan);
+  
+  
+  
+      // Data  - they are used to store small amounts of data
+      byte dan3 = 126;
+  
+      //defining long
+      long dan2 =  100000000l;
+      System.out.println("long : " + dan2);
+    }
+  }
